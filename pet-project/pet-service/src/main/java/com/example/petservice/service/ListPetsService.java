b02d1cfa87c159c4e1d687c799869492fe69828d
coztde <138366562+coztde@users.noreply.github.com>
2025-11-21 21:14:13 +0800

    添加宠物筛选修bug

--- pet-project/pet-service/src/main/java/com/example/petservice/service/ListPetsService.java
+++ pet-project/pet-service/src/main/java/com/example/petservice/service/ListPetsService.java
@@ -1,6 +1,7 @@
 package com.example.petservice.service;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.petpojo.dto.PetQueryDto;
 import com.example.petpojo.entity.Pets;
 import com.example.petpojo.vo.PetListVo; // 更改导入的VO类
 import com.example.petpojo.vo.PetsDetailsVo; // 导入详情VO类
@@ -13,11 +14,10 @@ public interface ListPetsService extends IService<Pets>{
 
     /**
      * 分页查询宠物列表
-     * @param currentPage 当前页码
-     * @param pageSize 每页数量
+     * @param petQueryDto 查询条件
      * @return 宠物列表分页对象
      */
-        IPage<PetListVo> listPets(Integer currentPage, Integer pageSize);
+        IPage<PetListVo> listPets(PetQueryDto petQueryDto);
         
     /**
      * 根据ID查询宠物详情
